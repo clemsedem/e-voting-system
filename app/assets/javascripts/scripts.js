96f3713f3491163c8d1cc3f3430c669dd631f40c
Clem Sedem <clemsedem@gmail.com>
2018-02-16 11:49:56 +0000

    sidebar

---
+++ app/assets/javascripts/scripts.js
@@ -0,0 +1,5 @@
+function w3_close() {
+  document.getElementById("main").style.marginLeft = "0%";
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
+}
